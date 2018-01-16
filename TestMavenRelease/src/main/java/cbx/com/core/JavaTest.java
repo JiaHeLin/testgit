59288a3de7cfcf331bd08abf7bcecf62a90c540f
jack.lin <jack.lin@cbxsoftware.com>
2018-01-16 13:38:27 +0800

    Test

---
+++ TestMavenRelease/src/main/java/cbx/com/core/JavaTest.java
@@ -0,0 +1,19 @@
+// Copyright (c) 1998-2018 Core Solutions Limited. All rights reserved.
+// ============================================================================
+// CURRENT VERSION CNT.5.0.1
+// ============================================================================
+// CHANGE LOG
+// CNT.5.0.1 : 2018-XX-XX, jack.lin, creation
+// ============================================================================
+package cbx.com.core;
+
+/**
+ * @author jack.lin
+ *
+ */
+public class JavaTest {
+
+    public static void main(final String[] args){
+        System.out.println("Test");
+    }
+}
